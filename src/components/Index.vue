e2e6aaac5034942c10fac8882cea0349cb4fbc6c
Alexander Ivchenko <A.S.Ivchenko@yandex.ru>
2017-12-07 15:02:25 +0300

    add description to index

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,5 +1,11 @@
 <template>
-
+  <v-container>
+    <v-layout>
+      <v-flex xs12 text-xs-center sm10 offset-sm1 md8 offset-md2 lg6 offset-lg3>
+        <h4 class="display-1">This platform is created to help you to develop good habits and get rid of bad ones</h4>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
